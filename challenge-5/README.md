402d338108aedb1412b950a0024f76ed838948eb
eliasjz36 <bseliasjimenez14@gmail.com>
2022-03-23 00:22:38 -0300

    Add challenge-5

---
+++ challenge-5/README.md
@@ -0,0 +1,1 @@
+# Prefiero utilizar PUG ya que me permite tener un codigo mas limpio
---
+++ challenge-5/ejs/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+const data = [];
+
+app.get('/', (req, res) => {
+	res.render('form', { data });
+});
+
+app.get('/products', (req, res) => {
+	res.render('products', { data });
+});
+
+app.post('/products', (req, res) => {
+	data.push(req.body);
+	res.render('form', { data });
+});
+
+app.listen(8080, () => console.log('ready'));
